cf409044bcd07b291cb3fa843cb8a8d1d3733fad
Desman012 <desmandwisaputra@gmail.com>
2022-08-19 15:45:01 +0700

    commited

---
+++ bodyJson.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/location',methods=[ 'GET'])
+def location_api():
+    kecepatan = request.args.get('kecepatan')
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+    timestamp = datetime.datetime.now()
+    return{
+        'kecepatan': kecepatan,
+        'latitude': latitude,
+        'longitude': longitude,
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
